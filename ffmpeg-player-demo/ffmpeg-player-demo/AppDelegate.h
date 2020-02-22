43815bac91e079a206ad1c3972c67f5d007c903f
midoks <midoks@163.com>
2020-02-23 03:11:37 +0800

    init

---
+++ ffmpeg-player-demo/ffmpeg-player-demo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ffmpeg-player-demo
+//
+//  Created by midoks on 2020/2/23.
+//  Copyright © 2020 midoks. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ ffmpeg-player-demo/ffmpeg-player-demo/AppDelegate.m
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.m
+//  ffmpeg-player-demo
+//
+//  Created by midoks on 2020/2/23.
+//  Copyright © 2020 midoks. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+@end
